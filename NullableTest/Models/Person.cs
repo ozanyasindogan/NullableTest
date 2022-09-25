ec08241d22a824bb311f5afe1ea7e3b19dc1096f
truckng <truckng@hotmail.com>
2022-09-26 01:05:28 +0300

    Add project files.

---
+++ NullableTest/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTest.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Title { get; set; }
+
+        [AllowNull]
+        public string? AllowNullString { get; set; }
+
+        [MaybeNull]
+        public string? MaybeNullString { get; set; }
+
+        [DisallowNull]
+        public string? DisallowNullString { get; set; }
+
+        public string Comments { get; set; } = "Test comments";
+
+        public int? Age { get; set; }
+        public double? LocationX { get; set; }
+        public float? LocationY { get; set; }
+        public long? ChildrenCount { get; set; }
+        public decimal? MoneyInTheBank { get; set; }
+        public ulong? SiblingsCount { get; set; }
+        
+    }
+}
---
+++ NullableTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Xml.Linq;
+
+bool IsNullable(Type type) => Nullable.GetUnderlyingType(type) != null;
+
+Assembly sharedAssembly = Assembly.GetExecutingAssembly();
+var models = sharedAssembly.DefinedTypes.Where(t => t.Namespace == "NullableTest.Models");
+
+foreach (var model in models)
+{
+    int n = 1;
+
+    foreach (var property in model.DeclaredProperties)
+    {
+        var type = property.PropertyType;
+        Console.WriteLine($"{n}. {type.FullName}, Nullable: {IsNullable(type)}");
+        n++;
+    }
+}
